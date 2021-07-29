906eaa2d3b64b3d906139c8fc13ac522470b4a81
Xing Jin (IFAG IT DAS RD PE) <Jin.Xing@infineon.com>
2021-07-29 20:20:25 +0200

    2907 2

--- main.py
+++ main.py
@@ -51,22 +51,42 @@ class VideoModel(db.Model):
 	def __repr__(self):
 		return f"Video(name = {name}, views = {views}, likes = {likes})"
 
+class CustomerModel(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(100), nullable=False)
+	balance = db.Column(db.Float, nullable=True)
+
+	def __repr__(self):
+		return f"Customer(id = {id}, name = {name}, balance = {balance})"
+
 #db.create_all()
+
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
 video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes on the video", required=True)
-
 video_update_args = reqparse.RequestParser()
 video_update_args.add_argument("name", type=str, help="Name of the video is required")
 video_update_args.add_argument("views", type=int, help="Views of the video")
 video_update_args.add_argument("likes", type=int, help="Likes on the video")
-
 resource_fields = {
 	'id': fields.Integer,
 	'name': fields.String,
 	'views': fields.Integer,
 	'likes': fields.Integer
+}
+
+customer_put_args = reqparse.RequestParser()
+customer_put_args.add_argument("name", type=str, help="Name of the customer is required", required=True)
+customer_put_args.add_argument("balance", type=float, help="Balance of the customer", required=True)
+customer_update_args = reqparse.RequestParser()
+customer_update_args.add_argument("name", type=str, help="Name of the customer is required")
+customer_update_args.add_argument("balance", type=float, help="Balance of the customer")
+
+resource_fields_customer = {
+	'id': fields.Integer,
+	'name': fields.String,
+	'balance': fields.Float
 }
 
 class Video(Resource):
@@ -114,7 +134,7 @@ class Video(Resource):
 
 	# def post(self, video_id):
 	# 	some_json = request.get_json()
-		
+
 	def delete(self, video_id):
 		#abort_if_video_id_doesnt_exist(video_id)
 		result = VideoModel.query.filter_by(id=video_id).first()
@@ -122,8 +142,61 @@ class Video(Resource):
 		db.session.commit()
 		return '', 204
 
+class Customer(Resource):
+	@marshal_with(resource_fields_customer)
+	def get(self, customer_id):
+		result = CustomerModel.query.filter_by(id=customer_id).first()
+		# drink = Drink.query.get_or_404(id)
+		if not result:
+			abort(404, message="Could not find customer with that id")
+		return result
+
+	@marshal_with(resource_fields_customer)
+	def get(self):
+		result = CustomerModel.query.all()
+		return result
+
+	@marshal_with(resource_fields_customer)
+	def put(self, customer_id):
+		args = customer_put_args.parse_args()
+		result = CustomerModel.query.filter_by(id=customer_id).first()
+		if result:
+			abort(409, message="Customer id taken...")
+
+		customer = CustomerModel(id=customer_id, name=args['name'], balance=args['balance'])
+		db.session.add(customer)
+		db.session.commit()
+		return customer, 201
+
+	@marshal_with(resource_fields_customer)
+	def patch(self, customer_id):
+		args = customer_update_args.parse_args()
+		result = CustomerModel.query.filter_by(id=customer_id).first()
+		if not result:
+			abort(404, message="Customer doesn't exist, cannot update")
+
+		if args['name']:
+			result.name = args['name']
+		if args['balance']:
+			result.balance = args['balance']
+
+		db.session.commit()
+		return result
+
+	# def post(self, video_id):
+	# 	some_json = request.get_json()
+	
+	def delete(self, customer_id):
+		#abort_if_video_id_doesnt_exist(video_id)
+		result = CustomerModel.query.filter_by(id=customer_id).first()
+		db.session.delete(result)
+		db.session.commit()
+		return '', 204
+
 api.add_resource(Video, "/video/<int:video_id>", "/videos")
 api.add_resource(HelloWorld, "/")
+
+api.add_resource(Customer, "/customer/<int:customer_id>", "/customers")
 
 if __name__ == "__main__":
     #sess = session.Session()
--- query.py
+++ query.py
@@ -2,13 +2,39 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-data = [{"likes":78, "name": 'Xing', "views":100000},
-        {"likes":10000, "name": 'Joe', "views":80000},
-        {"likes":35, "name": 'Tim', "views":2000}
+# data = [{"likes":78, "name": 'Xing', "views":100000},
+#         {"likes":10000, "name": 'Joe', "views":80000},
+#         {"likes":35, "name": 'Tim', "views":2000}
+# ]
+
+# for i in range(len(data)):
+#     response = requests.put(BASE + "video/" + str(i), data[i])
+#     print(response.json())
+
+
+# # input()
+# # response = requests.delete(BASE + "video/0")
+# # print(response)
+
+# input()
+# response = requests.get(BASE + "video/2")
+# print(response.json())
+
+# input()
+# response = requests.patch(BASE + "video/2", {"views":99})
+# print(response.json())
+
+# input()
+# response = requests.get(BASE + "video/2")
+# print(response.json())
+
+data = [{"name": 'Xing', "balance":100000.045},
+        {"name": 'Joe', "balance":80000.05},
+        {"name": 'Tim', "balance":2000.0}
 ]
 
 for i in range(len(data)):
-    response = requests.put(BASE + "video/" + str(i), data[i])
+    response = requests.put(BASE + "customer/" + str(i), data[i])
     print(response.json())
 
 
@@ -17,13 +43,13 @@ for i in range(len(data)):
 # print(response)
 
 input()
-response = requests.get(BASE + "video/2")
+response = requests.get(BASE + "customer/2")
 print(response.json())
 
 input()
-response = requests.patch(BASE + "video/2", {"views":99})
+response = requests.patch(BASE + "customer/2", {"balance":99})
 print(response.json())
 
 input()
-response = requests.get(BASE + "video/2")
+response = requests.get(BASE + "customer/2")
 print(response.json())
